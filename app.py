a46bfa9e2411de9f4d48c49e431cdc4d6506cca2
cindydao95 <cdao1295@gmail.com>
2023-07-28 12:59:42 -0500

    modified

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
 import pickle5 as pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, app, render_template
 import numpy as np
 import pandas as pd
-import json
 import os
 
 preprocessed_data_path = os.path.join("pickle_files","preprocessor.pkl")
 with open(preprocessed_data_path,"rb") as f:
     transform_data = pickle.load(f)
 
-with open('knn.pkl','rb') as f1:
+model_path  = os.path.join("pickle_files","knn.pkl")
+with open(model_path,'rb') as f1:
     knnmodel= pickle.load(f1)
 
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template('index.html')
+
 
 @app.route('/predictdata',method = ['GET','POST'])
 def predict_datapoint():
     if request.method=="GET":
-        return render_template('home.html')
+        return render_template('index.html')
     else:
         data = request.form
-        df_data = pd.read_json(data)
-        transfomred_input_data = transform_data.transform(df_data)
+        input_data_arr = np.array(list(data.values()))
+        transfomred_input_data = transform_data.transform(input_data_arr.reshape(1,-1))
         results = knnmodel.predict(transfomred_input_data)
-    return render_template('home.html',results = results[0])
+    return render_template('index.html',results = results[0])
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host = 0.0.0.0, debug=True)
